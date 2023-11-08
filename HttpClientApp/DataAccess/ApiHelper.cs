1fc1d3dbb07faaf66590d66bf61c5027bdf8eedd
BiancaGolik <bianka.golik@synergycodes.com>
2023-11-08 21:34:42 +0100

    Http Client - REST

---
+++ HttpClientApp/DataAccess/ApiHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+
+namespace HttpClientApp.DataAccess
+{
+    public class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("https://localhost:7049/api/");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
--- HttpClientApp/DataAccess/ProductData.cs
+++ HttpClientApp/DataAccess/ProductData.cs
@@ -1,32 +1,49 @@
 ﻿using HttpClientApp.Models;
 
-namespace RefitClient.DataAccess
+namespace HttpClientApp.DataAccess
 {
     public class ProductData
     {
-        Task<List<ProductModel>> GetProducts()
+        public static async Task<List<ProductModel>> GetProducts()
         {
-
+            var client = new HttpClient();
+            using (HttpResponseMessage response = await client.GetAsync("https://localhost:7049/api/Product"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<List<ProductModel>>();
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
         }
 
-        Task<ProductModel> GetProduct(int id)
+        public static async Task CreateProduct(ProductModel product)
         {
+            var client = new HttpClient();
 
+            using (HttpResponseMessage response = await client.PostAsJsonAsync("https://localhost:7049/api/Product", product))
+            {
+                response.EnsureSuccessStatusCode();
+            }
         }
 
-        Task CreateProduct(ProductModel product)
+        public static async Task UpdateProduct(int id, ProductModel product)
         {
+            var client = new HttpClient();
 
+            using (HttpResponseMessage response = await client.PutAsJsonAsync($"https://localhost:7049/api/Product/{id}", product))
+            {
+                 response.EnsureSuccessStatusCode();
+            }
         }
 
-        Task UpdateProduct(int id, ProductModel product)
+        public static async Task DeleteProduct(int id)
         {
-
-        }
-
-        Task DeleteProduct(int id)
-        {
-
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.DeleteAsync($"https://localhost:7049/api/Product/{id}");
         }
     }
 }
