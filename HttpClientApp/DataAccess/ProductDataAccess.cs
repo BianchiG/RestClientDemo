be0be225bc4f34c0eddd06bd91a213b5da300ae5
BiancaGolik <bianka.golik@synergycodes.com>
2023-11-08 23:24:06 +0100

    Http Client - typed client

---
+++ HttpClientApp/DataAccess/ProductDataAccess.cs
@@ -0,0 +1,51 @@
+﻿using HttpClientApp.DataAccess;
+using HttpClientApp.Models;
+using System.Text.Json;
+
+namespace HttpClientApp.DataAccess
+{
+    public class ProductClientService
+    {
+        private readonly HttpClient client;
+        public ProductClientService(HttpClient _client)
+        {
+            client = _client;
+        }
+
+        public async Task<List<ProductModel>> GetProducts()
+        {
+            using (HttpResponseMessage response = await client.GetAsync("Product"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<List<ProductModel>>();
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public async Task CreateProduct(ProductModel product)
+        {
+            using (HttpResponseMessage response = await client.PostAsJsonAsync("Product", product))
+            {
+                response.EnsureSuccessStatusCode();
+            }
+        }
+
+        public async Task UpdateProduct(int id, ProductModel product)
+        {
+            using (HttpResponseMessage response = await client.PutAsJsonAsync($"Product/{id}", product))
+            {
+                 response.EnsureSuccessStatusCode();
+            }
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync($"Product/{id}");
+        }
+    }
+}
--- HttpClientApp/Program.cs
+++ HttpClientApp/Program.cs
@@ -2,12 +2,24 @@ using HttpClientApp;
 using HttpClientApp.DataAccess;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Polly;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-ApiHelper.InitializeClient();
+// ApiHelper.InitializeClient();
+//builder.Services.AddHttpClient("ProductRestClient", client =>
+//{
+//    client.BaseAddress = new Uri("https://localhost:7049/api/");
+//    client.DefaultRequestHeaders.Clear();
+
+//});
+builder.Services.AddHttpClient<ProductClientService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7049/api/");
+    client.DefaultRequestHeaders.Accept.Clear();
+}).AddTransientHttpErrorPolicy(builder => builder.RetryAsync(3));
 
 await builder.Build().RunAsync();
